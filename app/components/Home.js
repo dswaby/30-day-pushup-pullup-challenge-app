e638fb60c30eb8c8d1d9d0159a724236c13078af
Danny Swaby <Danny@Swa.by>
2016-03-25 13:52:23 -0500

    design changes

--- app/components/Home.js
+++ app/components/Home.js
@@ -49,25 +49,34 @@ class Home extends React.Component {
     render() {
         return (
             <div className="home">
-                <div className="col-sm-12">
-                    <div className="col-sm-6">
-                        { this.state.errorText  && <ErrorCode errorCode={ this.state.errorText } /> }
-                        <UserForm handleUserForm={this.userLogin.bind( this )} buttonText="Login"/>
-                         <p style={{paddingTop: 10}}>
-                            <Link to="/register">Not a user? Sign up and start the challenge today!</Link>
-                        </p>
-                        <p style={{paddingTop: 10}}>
-                            <Link to="/reset">Forgot Password!</Link>
-                        </p>
+                <div className="home-section">
+                    <div className="container">
+                        <div className="col-sm-12">
+                        <div className="col-sm-6">
+                            { this.state.errorText  && <ErrorCode errorCode={ this.state.errorText } /> }
+                            <UserForm handleUserForm={this.userLogin.bind( this )} buttonText="Login"/>
+                             <p style={{paddingTop: 10}}>
+                                <Link to="/register">Not a user? Sign up and start the challenge today!</Link>
+                            </p>
+                            <p style={{paddingTop: 10}}>
+                                <Link to="/reset">Forgot Password!</Link>
+                            </p>
+                        </div>
+                        <div className="col-sm-6">
+                            <h2 className="text-center">Login to continue tracking your progress</h2>
+                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/94518/pushups.jpg" className="center-block img-responsive"/>
+                        </div>
+                        </div>
                     </div>
-                    <div className="col-sm-6">
-                        <h2 className="text-center">Login to continue tracking your progress</h2>
-                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/94518/pushups.jpg" className="center-block img-responsive"/>
-                    </div>
+                    
                 </div>
-                 <div className="col-sm-12">
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                 </div>
+                <div className="info-section">
+                 <div className="container">
+                 <div className="col-sm-12" style={{marginTop: 30, paddingBottom: 100}}>
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                     </div>
+                </div>
+                </div>
             </div>
         )
     }
